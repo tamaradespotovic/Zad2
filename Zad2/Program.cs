285cf44ac925366b917113aaecb9b862f6b5ae0f
Tamara Despotovic <tamaradespotovic@hotmail.com>
2021-04-12 00:18:13 +0200

    Add project files.

---
+++ Zad2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.IO;
+
+namespace Zad2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load("XMLFile.xml");
+            XmlElement root = doc.DocumentElement;
+            XmlNodeList nodes = root.SelectNodes("InterestRates/InterestRate");
+            doc.CreateElement("Description");
+            int step = 0;
+            foreach (XmlNode node in nodes) {
+                if (step == 0)
+                {
+                    string code = node["Code"].InnerText;
+                    node["Description"].InnerText = code;
+                    step = 1;
+                }
+                else {
+                    step = 0;
+                }  
+            }
+
+            doc.Save(Console.Out);
+            Console.Read();
+
+        }
+    }
+}
